7291b30c5cfa88a81d7a4ffe739f81f1ec3d99ea
Daianatk <daiana.campos04@gmail.com>
2019-04-17 08:51:32 -0500

    Cambios en codigo

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
-# Hackathon_Movies
-Hackathon Final Common Core
+# **MovieThon**
+### MovieThon es una página web en la que podrás encontrar toda la información necesaria sobre películas para iniciar esa maratón que tanto estabas postergando.
 
+# Proceso de definición de usuario
+Luego de observar la data a trabajar como equipo Front-End y UX, se decidió por orientar la aplicación web jóvenes adultos que prefieran ver sagas y/o trilogías. 
+
+Para lo cual se realizó una encuesta en la cuál preguntamos si les interesaría hacer una maratón de películas si tuvieran la oportunidad a lo cual más del 82% respondió afirmativamente.
+
+![chart](src/img/chart1.png)
+
+Además, preguntamos sobre los géneros que más verían en una maratón de películas.
+
+![chart](src/img/chart.png)
+## Usuario definido
+Se seleccionó como usuario a **jóvenes de 17 a 35 años interesados en buscar información para hacer una maratón de películas**
 
 # Historias de Usuarios
+### 1) “Yo, como usuari@, quiero poder visualizar las portadas y títulos de las películas que el sitio me ofrece"
+### 2) "Yo, como usuari@ quiero poder filtrar las películas por su género".
+### 3. “Yo como usuari@ quiero ver las películas filtradas/agrupadas por la  saga a la que pertenece”
+### 4)  "Yo, como usuari@, al seleccionar la película quiero ver la sinopsis, país, director, año de estreno y rating"
+
+# Iteraciones
+### 1era Iteración:
+
+Se recibió feedback sobre:
+
+Se cambió:
+
+### 2da Iteración:
+Se recibió feedback sobre:
+
+Se cambió:
+# Prototipado
+#### Prototipo de baja fidelidad
+![baja fidelidad](src/img/baja.jpg)
+
+#### Prototipo de alta fidelidad
+![alta fidelidad](src/img/pantalla1.png)
+![alta fidelidad](src/img/pantalla2.png)
+![alta fidelidad](src/img/pantalla3.png)
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,13 @@
-
-const buttonWelcome=document.getElementById('button-welcome');
+// Aqui debe ir el manejo del Dom
+/*const buttonWelcome=document.getElementById('button-welcome');
 const pageWelcome=document.getElementById('page-welcome');
 const home =document.getElementById('home');
 const pageHome=document.getElementById('page-home');
-const pageGender=document.getElementById('page-movies');
-const gender=document.getElementById('gender');
 const header=document.getElementById('logo-box');
-const footer=document.getElementById('footer');
+const footer=document.getElementById('footer');*/
 
-
-
-
-let pages = (pageToShow) => {
-  [pageWelcome, pageHome, pageGender].forEach(page => {
+/*const pages = (pageToShow) => {
+  [pageWelcome, pageHome].map(page => {
     page.classList.add('hide');
     page.classList.remove('show')
   })
@@ -26,47 +21,16 @@ buttonWelcome.addEventListener('click', () => {
   header.classList.remove('hide')
   footer.classList.add('show')
   footer.classList.remove('hide')
-});
+});*/
 
-home.addEventListener('click', () => {
-  pages(pageHome);
-});
-
-gender.addEventListener('click', () => {
-  pages(pageGender);
-});
-
-
-// Aqui debe ir el manejo del Dom
-// Probando Data desde OMDB, solo funcion Data con ¿console Log?
-
-let cardCreate=(data)=>{
-  return   `
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="card">
-                  <img class="card-img-top" src="${data.Poster}" alt="Card image cap">
-                  <div class="card-body">
-                  <h5 class="card-title">${data.Title}</h5>
-                  <p>Country : ${data.Country}</p>
-                  <p>Year : ${data.Year}  </p>
-                  <p>Director : ${data.Director}</p>
-                  <p>${data.Plot}</p>
-                  <p>IMDB : ${data.imdbRating}</p>
-                  </div>
-          </div>
-    </div> `
-    }
-
-// Probando Data desde OMDB, solo funcion Data con ¿console Log?
+// Probando Data desde OMDB.
 
 const all = document.getElementById('all')
 
 const dataMovie =['tt1457767','tt3065204','tt7069210','tt0217869', 'tt4972582', 'tt6823368','tt0195714', 'tt0309593', 'tt0414982', 'tt1144884', 'tt1622979', 'tt1179904', 'tt1536044', 'tt1778304', 'tt2109184','tt2473682', 'tt2473510', 'tt1728179', 'tt0295297', 'tt0304141', 'tt0330373', 'tt0373889', 'tt0417741', 'tt0926084',  'tt1201607', 'tt0120737', 'tt0167261',  'tt0167260','tt0121765', 'tt0121766', 'tt0120915', 'tt2488496', 'tt0086190', 'tt0080684', 'tt0076759', 'tt0088763', 'tt0096874', 'tt0099088', 'tt0848228', 'tt2395427', 'tt4154756','tt0325980','tt0383574','tt0449088','tt1298650','tt1790809','tt1099212', 'tt1259571', 'tt1325004', 'tt1324999', 'tt1673434', 'tt2322441', 'tt4465564', 'tt4477536', 'tt0126029', 'tt0298148', 'tt0413267', 'tt0892791','tt0114709', 'tt0120363', 'tt0435761']
 
-
-
-const randomMovie = dataMovie.forEach( item => {
-fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
+const randomMovie = dataMovie.map((item) => {
+fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
 .then(res => res.json())
  .then(data => {
  document.getElementById('allMovies').innerHTML += cardCreate(data);
@@ -76,8 +40,8 @@ fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
 })
 
 all.addEventListener('click', () => {
-  dataMovie.forEach( item => {
-    fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
+  dataMovie.map((item) => {
+    fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
     .then(res => res.json())
      .then(data => {
      document.getElementById('allMovies').innerHTML += cardCreate(data);
@@ -88,14 +52,32 @@ all.addEventListener('click', () => {
 }
 )
 
+cardCreate=(data)=>{
+  return   `
+        <div class="col-12 col-sm-6 col-md-3">
+          <div class="card">
+                  <img class="card-img-top" src="${data.Poster}" alt="Poster">
+                  <div class="card-body">
+                  <h5 class="card-title">${data.Title} (${data.Year}) </h5>
+                  <strong><p> ${data.Runtime}</p></strong>
+                  <p><strong>Country:</strong> ${data.Country}</p>
+                  <p><strong>Plot:</strong> ${data.Plot}</p>
+                  <p><strong>Director:</strong> ${data.Director}</p>
+                  <p><strong>Casting:</strong> ${data.Actors}</p>
+                  <p><strong>Rating:</strong> ${data.imdbRating}</p>
+                  <p><a target="_blank" href="https://www.youtube.com/watch?v=wkph6JOqDfA">Trailer</a></p>
+                  </div>
+          </div>
+    </div> `
+}
 
 //suspense Movies
 
 const arraySuspenseMovies =['tt1457767','tt3065204','tt7069210','tt0217869', 'tt4972582', 'tt6823368']
 
-let suspenseMovies = () =>{
-  arraySuspenseMovies.forEach( item => {
- fetch('https://www.omdbapi.com/?i='+ item +'&apikey=cc1c0d5d')
+const suspenseMovies = () =>{
+  arraySuspenseMovies.map((item) => {
+ fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
    .then(res => res.json())
     .then(data => {
   document.getElementById('allMovies').innerHTML += cardCreate(data);
@@ -110,15 +92,12 @@ document.getElementById("suspense").addEventListener("click", () => {
   document.getElementById('allMovies').innerHTML  = suspenseMovies();
 })
 
-
-
-
 ///Horror Movies
 
 const arrayHorrorMovies =['tt0195714','tt0309593', 'tt0414982', 'tt1144884', 'tt1622979', 'tt1179904', 'tt1536044', 'tt1778304', 'tt2109184', 'tt2473682', 'tt2473510', 'tt1728179']
 
 const horrorMovies = () =>{
-  arrayHorrorMovies.forEach( item => {
+  arrayHorrorMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
@@ -136,7 +115,7 @@ const horrorMovies = () =>{
 const arrayFantasyMovies =['tt0241527', 'tt0295297', 'tt0304141', 'tt0330373', 'tt0373889', 'tt0417741', 'tt0926084', 'tt1201607','tt0120737','tt0167261','tt0167260']
 
 const fantasyMovies = () =>{
-  arrayFantasyMovies.forEach( item => {
+  arrayFantasyMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
@@ -154,7 +133,7 @@ const fantasyMovies = () =>{
 const arrayScyfiMovies =['tt0121765','tt0121766','tt0120915','tt2488496','tt0086190','tt0080684','tt0076759','tt0088763','tt0096874','tt0099088']
 
 const scyfiMovies = () =>{
-  arrayScyfiMovies.forEach( item => {
+  arrayScyfiMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
@@ -172,7 +151,7 @@ const scyfiMovies = () =>{
 const arrayAccionMovies =['tt0848228','tt2395427','tt4154756','tt0325980','tt0383574','tt0449088','tt1298650','tt1790809']
 
 const accionMovies = () =>{
-  arrayAccionMovies.forEach( item => {
+  arrayAccionMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
@@ -190,7 +169,7 @@ const accionMovies = () =>{
 const arrayRomanceMovies =['tt1099212','tt1259571','tt1325004','tt1324999','tt1673434','tt2322441','tt4465564','tt4477536']
 
 const romanceMovies = () =>{
-  arrayRomanceMovies.forEach( item => {
+  arrayRomanceMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
@@ -208,7 +187,7 @@ const romanceMovies = () =>{
 const arrayAnimesMovies =['tt0126029','tt0298148','tt0413267','tt0892791','tt0114709','tt0120363','tt0435761']
 
 const animesMovies = () =>{
-  arrayAnimesMovies.forEach( item => {
+  arrayAnimesMovies.map((item) => {
   fetch('https://www.omdbapi.com/?i='+ item +'&apikey=9549174c')
   .then(res => res.json())
   .then(data => {
