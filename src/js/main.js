22ff9516502ed64c1c1d6e9b4aad7d18badee454
Daianatk <daiana.campos04@gmail.com>
2019-04-17 06:18:32 -0500

    Modificando funciones

--- src/js/main.js
+++ src/js/main.js
@@ -26,34 +26,7 @@ buttonWelcome.addEventListener('click', () => {
   footer.classList.remove('hide')
 });
 
-// Probando Data desde OMDB.
-
-const all = document.getElementById('all')
-
-const dataMovie =['tt1457767','tt3065204','tt7069210','tt0217869', 'tt4972582', 'tt6823368','tt0195714', 'tt0309593', 'tt0414982', 'tt1144884', 'tt1622979', 'tt1179904', 'tt1536044', 'tt1778304', 'tt2109184','tt2473682', 'tt2473510', 'tt1728179', 'tt0295297', 'tt0304141', 'tt0330373', 'tt0373889', 'tt0417741', 'tt0926084',  'tt1201607', 'tt0120737', 'tt0167261',  'tt0167260','tt0121765', 'tt0121766', 'tt0120915', 'tt2488496', 'tt0086190', 'tt0080684', 'tt0076759', 'tt0088763', 'tt0096874', 'tt0099088', 'tt0848228', 'tt2395427', 'tt4154756','tt0325980','tt0383574','tt0449088','tt1298650','tt1790809','tt1099212', 'tt1259571', 'tt1325004', 'tt1324999', 'tt1673434', 'tt2322441', 'tt4465564', 'tt4477536', 'tt0126029', 'tt0298148', 'tt0413267', 'tt0892791','tt0114709', 'tt0120363', 'tt0435761']
-
-const randomMovie = dataMovie.map((item) => {
-fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
-.then(res => res.json())
- .then(data => {
- document.getElementById('allMovies').innerHTML += cardCreate(data);
-
-})
-
-})
-
-all.addEventListener('click', () => {
-  dataMovie.map((item) => {
-    fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
-    .then(res => res.json())
-     .then(data => {
-     document.getElementById('allMovies').innerHTML += cardCreate(data);
-    
-    })
-    
-    })
-}
-)
+// Creando las Cards
 
 cardCreate=(data)=>{
   return   `
@@ -72,6 +45,34 @@ cardCreate=(data)=>{
           </div>
     </div> `
 }
+
+// Probando Data desde OMDB.
+
+const dataMovie =['tt1457767','tt3065204','tt7069210','tt0217869', 'tt4972582', 'tt6823368','tt0195714', 'tt0309593', 'tt0414982', 'tt1144884', 'tt1622979', 'tt1179904', 'tt1536044', 'tt1778304', 'tt2109184','tt2473682', 'tt2473510', 'tt1728179', 'tt0295297', 'tt0304141', 'tt0330373', 'tt0373889', 'tt0417741', 'tt0926084',  'tt1201607', 'tt0120737', 'tt0167261',  'tt0167260','tt0121765', 'tt0121766', 'tt0120915', 'tt2488496', 'tt0086190', 'tt0080684', 'tt0076759', 'tt0088763', 'tt0096874', 'tt0099088', 'tt0848228', 'tt2395427', 'tt4154756','tt0325980','tt0383574','tt0449088','tt1298650','tt1790809','tt1099212', 'tt1259571', 'tt1325004', 'tt1324999', 'tt1673434', 'tt2322441', 'tt4465564', 'tt4477536', 'tt0126029', 'tt0298148', 'tt0413267', 'tt0892791','tt0114709', 'tt0120363', 'tt0435761']
+
+const randomMovie = dataMovie.map( function(item) {
+  fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
+    .then(res => res.json())
+     .then(data => {
+  
+     document.getElementById('allMovies').innerHTML += cardCreate(data);
+  })
+})
+
+//All movie
+
+const all = document.getElementById('all');
+all.addEventListener('click', () => {
+  dataMovie.map((item) => {
+    fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=9549174c')
+    .then(res => res.json())
+     .then(data => {
+     document.getElementById('allMovies').innerHTML += cardCreate(data);
+    
+    })
+    
+    })
+})
 
 //suspense Movies
 
@@ -201,4 +202,8 @@ const animesMovies = () =>{
   document.getElementById('allMovies').innerHTML  = '';
   document.getElementById('allMovies').innerHTML  = animesMovies();
 })
+
+
+
+
 
